eb64ab3c7a7e35a7f07db78e417933f81ea9d814
jhon2520 <63693418+jhon2520@users.noreply.github.com>
2022-05-05 11:23:53 -0500

    estructura para un proyecto react

---
+++ src/MyApp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {colors} from './consts'
+
+const MyApp = () => {
+
+    console.log(colors.primary);
+    console.log(colors.secondary);
+
+
+    return (
+        <div>MyApp</div>
+    )
+}
+
+export default MyApp
---
+++ src/consts/colors.js
@@ -0,0 +1,5 @@
+export const colors = {
+    primary: "#454545",
+    secondary:"#451010"
+}
+
